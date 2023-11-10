0446050e99e28a0c03d6308918a16295ac6bd9b9
Cherubin EJ <cherubin@nowhere.co.jp>
2023-11-10 11:38:03 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is a continuity and improvement of a long personal modular project. It aims to agregrate diverse concepts and ideas of different challenges in autonomous driving',
+    author='Cherubin Mugisha',
+    license='MIT',
+)
